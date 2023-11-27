31ffa1a4038d6962a1ce59658759487c4a8eb97f
Jorge Benitez <a_jorge.benitez.llado@iespablopicasso.es>
2023-11-27 08:50:42 +0100

    Añado fichero readme y gitignore

---
+++ README.md
@@ -0,0 +1,9 @@
+# Act3GIT_JorgeBenitez
+### Este es el repositorio que hemos creado en GIT para la tarea en clase con eclipse
+
+Estamos aprendiendo a trabajar con **GIT EN ECLIPSE** y para eso hemos creado este repositorio :boy: :mortar_board:
+
+### Hemos hecho esta tarea para: 
+- Ver como nos desenvolvemos con Giy y GitHib
+- Ver el dominio que tengo sobre Git y GitHub
+- Ponerme a prueba antes del examen
--- src/NumerosAleatorios.java
+++ src/NumerosAleatorios.java
@@ -1,9 +1,18 @@
+import java.util.Random;
 
 public class NumerosAleatorios {
+    public static void main(String[] args) {
+        mostrarNumerosAleatorios();
+    }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void mostrarNumerosAleatorios() {
+        Random random = new Random();
 
-	}
+        System.out.println("20 números aleatorios entre 1 y 10:");
 
+        for (int i = 0; i < 20; i++) {
+            int numeroAleatorio = random.nextInt(10) + 1; // Genera números entre 1 y 10
+            System.out.print(numeroAleatorio + " ");
+        }
+    }
 }
