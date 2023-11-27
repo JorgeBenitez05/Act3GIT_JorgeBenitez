d51c6764ba0a7cbde51c7fdcd75c00097a99dfd6
Jorge Benitez <a_jorge.benitez.llado@iespablopicasso.es>
2023-11-27 12:43:05 +0100

    Modifico el programa de números aleatorios

--- src/NumerosAleatorios.java
+++ src/NumerosAleatorios.java
@@ -8,10 +8,10 @@ public class NumerosAleatorios {
     public static void mostrarNumerosAleatorios() {
         Random random = new Random();
 
-        System.out.println("20 números aleatorios entre 1 y 10:");
+        System.out.println("30 números aleatorios entre 1 y 10:");
 
-        for (int i = 0; i < 20; i++) {
-            int numeroAleatorio = random.nextInt(10) + 1; // Genera números entre 1 y 10
+        for (int i = 0; i < 30; i++) {
+            int numeroAleatorio = random.nextInt(10) + 1;
             System.out.print(numeroAleatorio + " ");
         }
     }
