1cb378ffea5cef5720cfdede93dab5a8a65f9007
Jorge Benitez <a_jorge.benitez.llado@iespablopicasso.es>
2023-11-27 12:20:56 +0100

    Modifico el programa pidiendo un tercer número

--- src/NumerosEnteros.java
+++ src/NumerosEnteros.java
@@ -2,31 +2,40 @@ import java.util.Scanner;
 
 public class NumerosEnteros {
 
-  public static void main(String[] args) {
-	Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-    int primerNumero;
-    int segundoNumero;
-    
-    do {
-      System.out.print("Introduzca el primer número: ");
-      primerNumero = Integer.parseInt(scan.nextLine());
-      System.out.print("Introduzca el segundo número distinto al anterior: ");
-      segundoNumero = Integer.parseInt(scan.nextLine());
-      
-      if(primerNumero == segundoNumero) {
-        System.out.print("ERROR. Los números introducidos son iguales");
-      }
-    } while (primerNumero == segundoNumero);
-    
-    if (primerNumero > segundoNumero) {
-      int num = primerNumero;
-      primerNumero = segundoNumero;
-      segundoNumero = num;
+        int primerNumero;
+        int segundoNumero;
+        int tercerNumero;
+
+        do {
+            System.out.print("Introduzca el primer número: ");
+            primerNumero = Integer.parseInt(scan.nextLine());
+
+            System.out.print("Introduzca el segundo número distinto al anterior: ");
+            segundoNumero = Integer.parseInt(scan.nextLine());
+
+            if (primerNumero == segundoNumero) {
+                System.out.print("ERROR. Los números introducidos son iguales");
+            }
+        } while (primerNumero == segundoNumero);
+
+        if (primerNumero > segundoNumero) {
+            int num = primerNumero;
+            primerNumero = segundoNumero;
+            segundoNumero = num;
+        }
+
+        System.out.print("Introduzca un tercer número: ");
+        tercerNumero = Integer.parseInt(scan.nextLine());
+
+        for (int i = primerNumero; i <= segundoNumero; i += 7) {
+            System.out.println(i);
+        }
+
+        if (tercerNumero >= primerNumero && tercerNumero <= segundoNumero) {
+            System.out.println("El tercer número es: " + tercerNumero);
+        }
     }
-    
-    for(int i = primerNumero; i <= segundoNumero; i += 7) {
-      System.out.println(i);
-    }
-  }
 }
